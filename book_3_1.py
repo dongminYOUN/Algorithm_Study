0430f14a4f553bd77ac57e25d4deb0ce4559d589
dongminYOUN <kkang15794@naver.com>
2022-08-03 09:39:43 +0900

    0803_study_book

--- book_3_1.py
+++ book_3_1.py
@@ -4,7 +4,7 @@ n = int(input())
 #거스름돈 list
 charge_list = [500, 100, 50, 10]
 count = 0
-charge_dict = {}
+#charge_dict = {}
 
 # 몇개의 거스름돈이 나왔는지 dictionary로 저장해볼까
 # while문을 통해서 입력받은 값을 계속 감소시킴
@@ -12,7 +12,7 @@ while n != 0:
     if n >= 500:
         n = n - charge_list[0]        
         count += 1
-        #$charge_dict['500'] += 1 dictionary value값 증가 어떻게?
+        #charge_dict['500'] += 1 dictionary value값 증가 어떻게?
     elif n >= 100:
         n = n - charge_list[1]
         count += 1
@@ -25,7 +25,6 @@ while n != 0:
      
 
 print(count)
-print(charge_dict)
 
 #책 답안
 # for coin in list:
